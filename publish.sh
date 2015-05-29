8374a228a509333c067852f1da3e242f0e427e7e
Andrei McMillan <amcmillan@yieldbot.com>
2015-05-29 00:30:38 -0400

    update token

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 curl \
--H "Authorization: Bearer 4/hPtVoct8DQ9mXPn7s5Gf2K-xJMkCk1eHJ35F6nE-poM.4gvsMDn2Hwoa3oEBd8DOtNCR89U7mwI#"  \
+-H "Authorization: Bearer 4/hPtVoct8DQ9mXPn7s5Gf2K-xJMkCk1eHJ35F6nE-poM.4gvsMDn2Hwoa3oEBd8DOtNCR89U7mwI"  \
 -H "x-goog-api-version: 2" \
 -X PUT \
 -T src.zip \
