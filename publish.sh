621ff822a743e52ba4cb293733a6da95ddfb2e84
Andrei McMillan <amcmillan@yieldbot.com>
2015-05-29 00:19:02 -0400

    test out the publish api

---
+++ publish.sh
@@ -0,0 +1,7 @@
+curl \
+-H "Authorization: Bearer 4/sklftdj0i4LZvl7m0rADRtNZaJy662ZWIyvE1O3iD0w.oi_9SlMwyAoQ3oEBd8DOtNCfUrM7mwI"  \
+-H "x-goog-api-version: 2" \
+-X PUT \
+-T src.zip \
+-v \
+https://www.googleapis.com/upload/chromewebstore/v1.1/items/lcldgjakanbfmdgleoekbbelmaikolck
